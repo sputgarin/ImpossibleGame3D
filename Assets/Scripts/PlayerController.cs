4e594060f67bce2241dcc31ce2ddf25be6ed2184
sputgarin <sputgarin@protonmail.com>
2023-10-04 13:11:13 +0200

    Introduce Speed Field

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0,0,0.05f);
+        transform.Translate(0,0,speed  * Time.deltaTime);
     }
 }
