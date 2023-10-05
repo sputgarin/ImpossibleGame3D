208003368f4a5f2c171c7ca303ce42cca0a5a65a
sputgarin <sputgarin@protonmail.com>
2023-10-05 13:25:36 +0200

    Implemented Gravity & Out Of Bounds Mechanic
    Also added a FX pack

--- Assets/Scripts/PlayerControllerSimple.cs
+++ Assets/Scripts/PlayerControllerSimple.cs
@@ -4,31 +4,51 @@ using UnityEngine;
 public class PlayerControllerSimple : MonoBehaviour
 {
     public float forwardSpeed = 0.05f;
-    public int jumpForce = 5;
+    public float jumpForce = 5;
     private float horizontalInput;
     public float sideSpeed = 3f;
     private Vector3 movedirection;
 
-    private float initialGravityScale;
+    private bool isTouchingGround;
+    
     private ConstantForce cf;
-
+    private Vector3 forceDirection;
+    
     public Rigidbody rb;
+
+    [SerializeField] 
+    private float downwardForce;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cf = GetComponent<ConstantForce>();
+        forceDirection = new Vector3(0, 0, 0);
         
     }
 
 
     void Update()
     {
+        // Code In the classroom Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
         transform.Translate(0,0,forwardSpeed  * Time.deltaTime);
         Move();
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
         {
             JumpUp();
+        }
+
+        if (isTouchingGround == false && (rb.velocity.y > 0 || rb.velocity.y < 0))
+        {
+            Debug.Log("WAS TRIGGERED!!½!!!");
+            forceDirection.y = downwardForce;
+            cf.force = forceDirection;
+        }
+
+        else
+        {
+            forceDirection.y = 0;
+            cf.force = forceDirection;
         }
     }
 
@@ -47,6 +67,29 @@ public class PlayerControllerSimple : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         movedirection = new Vector3(horizontalInput , 0 ,0);
         
+    }
+    
+    // Used to check if the player is grounded.
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isTouchingGround = true;
+            
+            // Animation is not used for my character yet.
+            // anim.SetBool(IsLanded, true);
+            // Debug.Log("Player is grounded!");
+        }
+    }
+    
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isTouchingGround = false;
+            // anim.SetBool(IsLanded, false);
+            // Debug.Log("NO GROUND!");
+        }
     }
     
     
